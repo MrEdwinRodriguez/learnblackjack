736019be9531399b3a4b1f130412b61635df67e0
Edwin <mredwinrodriguez@gmail.com>
2021-01-06 13:44:51 -0500

    connect to server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+//connnect database
+connectDB();
+
 const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => res.send('API is running'));
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen(PORT, () => console.log(`Server Stated on port ${PORT}`));
