7f369de747bf43be2e80bb0467d55bb8d1eb8c3f
Edwin <mredwinrodriguez@gmail.com>
2021-01-15 17:02:43 -0500

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -20,6 +21,7 @@ const App = () => {
             <Route exact path='/login' component={Login} />
           </Switch>
         </section>
+        <Footer />
       </Fragment>
     </Router>
   );
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import '../../css/style.css';
+
+const Footer = () => {
+  return (
+    // <footer className="bg-royal footer text-white mt-5 p-4 text-center">
+    // Copyright &copy; { new Date().getFullYear()} Blue and White Connect
+    // </footer>
+    <div>
+      <div className='phantom' />
+      <div className="footer text-light mt-5 p-4 text-center" >
+          Copyright &copy; { new Date().getFullYear()} Learn Black Jack
+      </div>
+  </div>
+  )
+}
+export default Footer
