6e41f5dc16ff3f95292e252a319692be38702f56
Edwin <mredwinrodriguez@gmail.com>
2021-01-28 14:35:22 -0500

    fixed alert UI display

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ const App = () => {
         <Fragment>
           <Navbar />
           <Route exact path='/' component={Landing} />
-          <section className='container-fluid'>
+          <section className='container-fluid hold-alert'>
           <Alert />
             <Switch>
               <Route exact path='/register' component={Register} /> */}
