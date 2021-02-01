22f78cac0a1a58c08f873507626006577b319a22
Edwin <mredwinrodriguez@gmail.com>
2021-01-31 21:13:06 -0500

    dashboard buttons

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
-import React, {useEffect} from 'react'
+import React, {Fragment ,useEffect} from 'react'
 import PropTypes from 'prop-types';
+import {Link, Redirect} from 'react-router-dom';
 import  { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 
@@ -9,9 +10,25 @@ const Dashboard = ({getCurrentProfile, auth, profile}) => {
     }, []);
 
     return (
-        <div>
-            Dashboard
-        </div>
+        <Fragment>
+            <section className="landing">
+                <div className="dark-overlay">
+                    <div className='container'>
+                    <div className="signup-form">
+                        <div>
+                        <h2 className='text-primary-dashboard'>PLAY & LEARN</h2>
+                            <div class="btn-group-vertical">
+                                <button type="button" class="btn btn-primary">Play Black Jack</button>
+                                <button type="button" class="btn btn-primary">Learn Black Jack</button>
+                                <button type="button" class="btn btn-primary">Count Cards</button>
+                                <button type="button" class="btn btn-primary">Learn to Count</button>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
+        </Fragment>
     )
 }
 
