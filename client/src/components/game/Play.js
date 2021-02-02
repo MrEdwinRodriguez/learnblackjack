667b51fe2dc57e2186a48367363967deccbfdf8c
Edwin <mredwinrodriguez@gmail.com>
2021-02-02 10:25:33 -0500

    table base

--- client/src/components/game/Play.js
+++ client/src/components/game/Play.js
@@ -14,12 +14,13 @@ const Play = ({getCurrentProfile, auth, profile}) => {
             <section className="landing">
                 <div className="dark-overlay">
                     <div className='container'>
-                    <div className="signup-form">
-                        <div>
-                        <h2 className='text-primary-dashboard'>Play Black Jack</h2>
 
+                        <div className='table'>
+                        <h2 className='table-title'>BlackJack</h2>
+                        <h5 className='table-pays'>Pays 2 to 3</h5>
+                        <h5 className='table-dealer-stands'>Dealer Stand on 17</h5>
                         </div>
-                    </div>
+
                     </div>
                 </div>
             </section>
